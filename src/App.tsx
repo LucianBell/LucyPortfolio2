499d9b74f985bf27fae5e9f8f95ae6f2a8774705
Lucian Bellini <lucianbellini1@gmail.com>
2023-05-21 22:48:33 -0300

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { AiOutlineMenu } from "react-icons/ai";
+
+function App() {
+  return (
+    <>
+      <header>
+        <AiOutlineMenu />
+      </header>
+    </>
+  );
+}
+
+export default App;
